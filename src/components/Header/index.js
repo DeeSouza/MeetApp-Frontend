08be5d61df2e6411790c059bac9c2fb878deed9f
Diego Souza <dee-souza@hotmail.com>
2019-09-12 16:10:59 -0300

    Change: Link in logo header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ export default function Header() {
 	return (
 		<Container>
 			<Wrapper>
-				<Link to="/">
+				<Link to="/dashboard">
 					<img src={logo} alt="Meetapp - Networking Infinite" />
 				</Link>
 
