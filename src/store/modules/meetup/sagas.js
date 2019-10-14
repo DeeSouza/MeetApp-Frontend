a377cc0495e63dd34d83f36b73a22bdd5c96bc97
Diego Souza <dee-souza@hotmail.com>
2019-10-14 15:43:07 -0300

    :art: Path modules

--- src/store/modules/meetup/sagas.js
+++ src/store/modules/meetup/sagas.js
@@ -1,7 +1,7 @@
 import { takeLatest, call, all, put } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
-import api from '../../../services/api';
-import history from '../../../services/history';
+import api from '~/services/api';
+import history from '~/services/history';
 import {
 	meetUpdateSuccess,
 	meetUpdateFailure,
